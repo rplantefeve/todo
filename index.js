a5419bf4830e7664327a0c319c90aae66ae43cbf
R. Plantefève <romain.plantefeve@laposte.net>
2019-09-13 19:50:20 +0200

    Affichage (en dur) des tâches contenues dans la session utilisateur

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 var express = require('express');
+// pour gérer la session
+var session = require('express-session');
 
 // instanciation de l'application
 var app = express();
 
-// ajout d'une route GET (route par défaut)
-app.get('/', function(req, res) {
-    res.render('accueil.ejs');
+// date d'expiration du cookie de session : 1 heure
+var expiryDate = new Date(Date.now() + 60 * 60 * 1000);
+
+// tableau de test de choses à faire
+var thingsToDo = ['Couper du bois', 'Faire la vaisselle', 'Nourrir le cheval'];
+
+// Accepter les connexions sécurisées du localhost
+app.use(session({
+    name: 'session',
+    secret: 'Bob Morane',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: false,
+      expires: expiryDate
+    }
+  }))
+
+  // ajout d'une route GET (route par défaut)
+  .get('/', function(req, res, next) {
+    if (req.session.views) {
+      req.session.views++;
+    } else {
+      req.session.views = 1;
+    }
+
+    // Gestion du tableau de tâches
+    if (!req.session.thingsToDo) {
+      req.session.thingsToDo = thingsToDo;
+    }
+
+    res.render('accueil.ejs', {
+      views: req.session.views,
+      thingsToDo : req.session.thingsToDo
+    });
   })
 
   // gestion des routes inconnues
