6cf76dcdae4c89d9723badd68f0bbb8ba7d979a2
R. Plantefève <romain.plantefeve@laposte.net>
2019-09-16 10:35:30 +0200

    Amélioration mineure
    Ajout d'un middleware perso pour initialiser le tableau de tâches

--- index.js
+++ index.js
@@ -20,8 +20,7 @@ var dir = path.join(__dirname, 'public');
 // date d'expiration du cookie de session : 1 heure
 var expiryDate = new Date(Date.now() + 60 * 60 * 1000);
 
-// tableau de test de choses à faire
-var thingsToDo = ['Couper du bois', 'Faire la vaisselle', 'Nourrir le cheval'];
+
 
 app.use(express.static(dir)) // init du middleware pour servir des fichiers
 
@@ -35,23 +34,19 @@ app.use(express.static(dir)) // init du middleware pour servir des fichiers
       expires: expiryDate
     }
   }))
-
-  // ajout d'une route GET (route par défaut)
-  .get('/', function(req, res, next) {
-    if (req.session.views) {
-      req.session.views++;
-    } else {
-      req.session.views = 1;
-    }
-
+  // ajout d'un middleware perso pour initialiser le tableau de tâches
+  .use(function(req, res, next) {
     // Gestion du tableau de tâches
     if (!req.session.thingsToDo) {
       // Initialisation en dur
-      req.session.thingsToDo = thingsToDo;
+      req.session.thingsToDo = ['Couper du bois', 'Faire la vaisselle', 'Nourrir le cheval'];
     }
+    next();
+  })
 
+  // ajout d'une route GET (route par défaut)
+  .get('/', function(req, res, next) {
     res.render('accueil.ejs', {
-      views: req.session.views,
       thingsToDo: req.session.thingsToDo
     })
   })
@@ -79,12 +74,10 @@ app.use(express.static(dir)) // init du middleware pour servir des fichiers
     res.redirect('/');
   })
 
-/*
   // gestion des routes inconnues
   .use(function(req, res, next) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.status(404).send('Page introuvable !');
-  })*/
-;
+    // redirection vers l'accueil
+    res.redirect('/');
+  });
 
 app.listen(8080);
