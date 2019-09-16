30e91477df96fb5e8e5239bad50fb862e0d084c1
rplantefeve <romain.plantefeve@gmail.com>
2019-09-16 10:14:43 +0200

    Permière version de l'ahjout d'une tâche

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ var express = require('express');
 var path = require('path');
 // pour gérer la session
 var session = require('express-session');
+// pour gérer l'envoi du formulaire
+var parser = require('body-parser');
 
 // instanciation de l'application
 var app = express();
+
+// création du parser de la requête
+var urlencodedParser = parser.urlencoded({
+  extended: false
+})
 
 // emplacement des fichiers statiques (images, ...)
 var dir = path.join(__dirname, 'public');
@@ -39,6 +46,7 @@ app.use(express.static(dir)) // init du middleware pour servir des fichiers
 
     // Gestion du tableau de tâches
     if (!req.session.thingsToDo) {
+      // Initialisation en dur
       req.session.thingsToDo = thingsToDo;
     }
 
@@ -52,7 +60,22 @@ app.use(express.static(dir)) // init du middleware pour servir des fichiers
   .get('/delete/:id', function(req, res, next) {
     // Suppression de la tâche i du tableau de tâches
     req.session.thingsToDo.splice(req.params.id, 1);
-
+    // redirection vers l'accueil
+    res.redirect('/');
+  })
+  // Ajout de la route POST /add pur ajouter une tâche
+  .post('/add', urlencodedParser, function(req, res) {
+    let thingsToDo = req.session.thingsToDo;
+    let task = req.body.task;
+    // si la variable de session existe
+    if (thingsToDo) {
+      // et que la variable du formulaire task existe également
+      if (task) {
+        // ajout de la tâche au tableau
+        thingsToDo.push(task);
+      }
+    }
+    // redirection vers l'accueil
     res.redirect('/');
   })
 
